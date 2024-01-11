b3c7d1380faf504df64fb485abe495a07c22091f
ensarsnr <dev.anise@outlook.com>
2024-01-11 21:15:44 +0300

    Added hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
+import 'package:cumb/model/items_model.dart';
 import 'package:cumb/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(ItemModelAdapter());
+  await Hive.openBox<ItemModel>("itemBox");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // telefonun dönmesini engelliyor. Diğeri de tam ekran moduna çeviriyor.
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
