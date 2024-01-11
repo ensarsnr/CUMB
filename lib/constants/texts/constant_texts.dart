c94a9efb05f75bc45641074e39f4aeb3a7822210
ensarsnr <dev.anise@outlook.com>
2024-01-11 21:15:28 +0300

    Text

---
+++ lib/constants/texts/constant_texts.dart
@@ -0,0 +1,20 @@
+// Home sayfasında ki kelimeler
+class HomeText {
+  static const title = "CUMB";
+  static const readyWords = "Hazır Kelimeler";
+  static const ownWords = "Kendi oluşturdukların";
+  static const exit = "Çıkış";
+}
+
+// OwnWord dosyasında, popup'da bulunan kelimeler
+class OwnWordDialog {
+  static const String createdCard = "Kart oluştur";
+  static const String cardName = "Kart ismi";
+  static const String addCorrespondingWord = "Karşılık gelen";
+}
+
+// OwnWord sayfasına ait kelimeler
+
+class OwnWord {
+  static const String title = "Kendi Hazırladıkların";
+}
