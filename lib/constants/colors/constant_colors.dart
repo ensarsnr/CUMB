31b32bbb7563e99a2b536a489107a2541e486415
ensarsnr <dev.anise@outlook.com>
2024-01-11 21:15:08 +0300

    Colors

---
+++ lib/constants/colors/constant_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeButtonColors {
+  static const buttonColor = Color.fromARGB(249, 255, 151, 224);
+}
+
+class HomeColors {
+  static const bgColor = Color.fromARGB(255, 53, 53, 53);
+}
+
+class HomeNavColors {
+  static const navColors = Color.fromARGB(249, 255, 151, 224);
+}
